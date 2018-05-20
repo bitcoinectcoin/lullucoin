2df824236ddc25e1049928ef6df0eaba25de5984
nnian <35841252+orangecoi@users.noreply.github.com>
2018-05-20 20:52:36 +0200

    Testnet

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -80,9 +80,9 @@ const size_t   FUSION_TX_MAX_SIZE                            = CRYPTONOTE_BLOCK_
 const size_t   FUSION_TX_MIN_INPUT_COUNT                     = 12;
 const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
 
-const uint32_t UPGRADE_HEIGHT_V2                             = 40000;
-const uint32_t UPGRADE_HEIGHT_V3                             = 46000;
-const uint32_t UPGRADE_HEIGHT_V4                             = 4294967292;
+const uint32_t UPGRADE_HEIGHT_V2                             = 2;
+const uint32_t UPGRADE_HEIGHT_V3                             = 3;
+const uint32_t UPGRADE_HEIGHT_V4                             = 4;
 const uint32_t UPGRADE_HEIGHT_V5                             = 4294967293;
 const uint32_t UPGRADE_HEIGHT_V6                             = 4294967294;
 
@@ -141,17 +141,11 @@ const uint32_t P2P_IP_BLOCKTIME                              = (60 * 60 * 24);//
 const uint32_t P2P_IP_FAILS_BEFORE_BLOCK                     = 10;
 const uint32_t P2P_IDLE_CONNECTION_KILL_INTERVAL             = (5 * 60);      //5 minutes
 
-const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "37ceebc436f3004d3739499c67ccb730cc4734950f414cdb332b24c5ce764317";
+const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "37ceebc436f3004d3739499c67ccb730cc4734950f414cdb332b24c5deadbeaf";
 
 const char* const SEED_NODES[] = { 
-  "node-00.qwertycoin.org:8196",//00
-  "195.201.25.118:8196",        //01
-  "198.147.30.116:8196",        //explorer
-  "198.147.30.115:8196",        //mp
-  "node-02.qwertycoin.org:8196",//02
-  "78.47.85.215:8196",          //03
-  "46.38.242.66:8196",          //op
-  "195.201.27.148:8196"         //05
+  "46.38.242.66:8196",//00
+  "138.201.244.197:8196",        //01
 };
 
 struct CheckpointData {
@@ -160,25 +154,7 @@ struct CheckpointData {
 };
 
 const std::initializer_list<CheckpointData> CHECKPOINTS = { 
-  { 5000,"f44ad7e79c4dc4b8cce6100460444f8fc61096a6cf4d9a53107abde0b8750b4f"},
-  { 8473,"e131c96f6105604ea3f7dd2b55d73d430b28bed133112c9992a38c26b5239e14"},
-  {10000,"fb021fd69f78a60a365c16692777e7f699215404366545e072eba7dddbf1d61d"},
-  {15000,"fb8ad336e6518aa6a5f874a15276561249d36a35afd08f0f1fab78333d9e4149"},
-  {20000,"d32286163e2a5cfbbab35007438e7bf90564afee70c715930606710e96f2ce19"},
-  {25000,"c064cd86be01c32c86e0c7a6295023628acfeaa2786e2873a03343bc6f6a8d41"},
-  {30000,"1d32bbca2149eeb27ff9e2c19d1b6ace4a160764839a4da7031328f7ea49e6f6"},
-  {35000,"173269a8b2ed188ef95a876acaf807cbbc4817191f036063ecf92112f46b5cbf"},
-  {40000,"60a9694b18cf470bcfd9f36f32ad01f86b5538fe1f88a3bd2717ca6ad1c7ce80"},
-  {40001,"17132deac10a3661f56eb4b19dbaea77cbf2ef8a131f78df04c7df023b687158"},
-  {45000,"0e504e82e6fe247b0b5dae9fc5bd3e82982e61610fd674d8e1f8ee3c85000c15"},
-  {46000,"51e2f9e09278cb66b08efcecfdf3208053ee53030c6fc5a6a647565a6b318cd5"},
-  {46001,"15853b6a46d3fcf1554935dadc8988a0de95b0a9e711cfb845d6fe3fde5aa98a"},
-  {50000,"5599ca3d8ac58f377187b5a43c00103496244ec95299e4ae3dff3be6016488bf"},
-  {55000,"b90e836c7ad95a6184869f16406161ac541c4ee3f2436447668a7f43362da733"},
-  {60000,"796e0037bed22ec317dc165bdfcc331322fe3fcc7ca162827ab374a7d17107f0"},
-  {65000,"d03c367bc1d87bea553346245ae60865175371b449dea11b79b7cfa28453d892"},
-  {70000,"9b035668d72b17ceaabbd4b341089f090aa5ebad37f33513509d9f6e102c7c0c"},
-  {72446,"c4bf1cfc520e804a87a092c378faa0668de6f937d3b308237f5589899a7fe456"}
+
 };
 
 } // CryptoNote
